9c798b1f889f26168c18b0117fedd1a8953906dc
kim5537 <fudgml778@naver.com>
2024-07-10 23:19:46 +0900

    “240710h”

--- project1_aboutme/scrip.js
+++ project1_aboutme/scrip.js
@@ -29,3 +29,35 @@ topSlide.addEventListener("click", () => {
   const topImg = document.querySelector("#top_items");
   topImg.classList.toggle("active");
 });
+
+//각요소 마우스 오버
+// const illust1 = Document.querySelector(".Aillust");
+// console.log(illust1);
+// illust1.addEventListener("mouseover", () => {
+//   const scale1 = Document.querySelector(".back1");
+//   scale1.style.transform = "scale(1.2)";
+// });
+
+// Intersection Observer 객체 생성
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const bubble = entry.target.querySelector(".Abubble");
+        console.log(bubble);
+        // bubble.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+// // 관찰할 대상 요소 선택
+// const Aillust = document.querySelector(".Aillust");
+
+// // Intersection Observer에 관찰할 요소 등록
+// if (Aillust) {
+//   observer.observe(Aillust);
+// }
