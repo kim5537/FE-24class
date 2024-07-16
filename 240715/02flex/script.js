09ff81a585876a04d63b12692d9f6b5b59c7c887
kim5537 <fudgml778@naver.com>
2024-07-16 18:17:43 +0900

    240716

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -8,3 +8,40 @@ window.addEventListener("scroll", () => {
     header.classList.remove("active");
   }
 });
+
+//slick slider
+$(".myslider").slick({
+  dots: true,
+  infinite: false,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  reponsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
+});
