4cd7603fdfa52dad60ae7073bc5071fe158c4e9d
kim5537 <fudgml778@naver.com>
2024-07-17 16:50:57 +0900

    “240717”

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -1,12 +1,33 @@
+//Header nav
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
+  const gototop = document.querySelector(".gototop");
 
   if (scroll > 50) {
     header.classList.add("active");
+    gototop.classList.add("active");
   } else {
     header.classList.remove("active");
+    gototop.classList.remove("active");
   }
+});
+
+//Trriger
+const trigger = document.querySelector(".trigger");
+const gnb = document.querySelector(".gnb");
+const gnbLinks = gnb.querySelectorAll("a");
+
+trigger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+
+gnbLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    trigger.classList.remove("active");
+    gnb.classList.remove("active");
+  });
 });
 
 //slick slider
