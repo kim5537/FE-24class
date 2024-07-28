cc85c5695104a3c1b843e227d5801f443e258486
kim5537 <fudgml778@naver.com>
2024-07-29 00:07:02 +0900

    “240729”

--- 240723/script.js
+++ 240723/script.js
@@ -6,3 +6,12 @@ $(".history-slider").slick({
   slidesToShow: 4,
   slidesToScroll: 2,
 });
+
+// Slick Slider : Project
+$(".project-photo").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
