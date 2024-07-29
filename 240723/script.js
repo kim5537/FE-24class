6e9679f5e7eb56b7c849b7f3f1329674789e9950
kim5537 <fudgml778@naver.com>
2024-07-29 17:29:07 +0900

    240729

--- 240723/script.js
+++ 240723/script.js
@@ -1,3 +1,36 @@
+//header
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  let scrollY = window.scrollY;
+  if (scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+//trigger
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("click", function () {
+  const gnb = document.querySelector(".gnb");
+  const menus = document.querySelectorAll(".menu a");
+  // console.log(gnb);
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+
+  menus.forEach((menu) => {
+    menu.addEventListener("click", () => {
+      trigger.classList.remove("active");
+      gnb.classList.remove("active");
+    });
+  });
+});
+
+//scrollTo
+$(".menu a").click(function () {
+  $.scrollTo(this.hash || 0, 900);
+});
+
 //slick slider
 $(".history-slider").slick({
   dots: true,
@@ -5,6 +38,29 @@ $(".history-slider").slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 // Slick Slider : Project
---
+++ 240729/01js/script.js
@@ -0,0 +1,69 @@
+// const test1 = "mon";
+
+// let test2 = "mon";
+
+//재선언 = 불가능
+// let test2 = "tue";
+
+// 재할당 = 가능
+// test2 = "tue";
+
+//A 사람
+//카테고리 안에 삼품개수 10개
+// var caregory = 10;
+//B 사람
+//장바구니 에 담긴 상품개수
+// var caregory = 8;
+// 이래도 애러가 안 뜸
+
+// const x = 10;
+// const z = x + y;
+// const y = 20;
+
+// console.log(z);
+
+// var x = 10;
+// var z = x + y;
+// var y = 20;
+
+// console.log(z);
+//콘솔에 Nan이 뜸
+//not a Number 이란 뜻으로 자료의 형태중 하나임 = 오류가 아님,데이터값이 나온 것
+
+// document.querySelector("#wrapper").addEventListener("click", ()=>{
+//   선언은 없지만 가능하다. 선언을 하는 이유는 다른 곳에서도 사용할 수 있기 때문에 선언하여 값을 넣어준 것이다.
+// });
+
+// console.log(wrapper);
+
+//-----숫자형
+// const num1 = 1;
+// const num2 = 2;
+
+// const sum = num1 + num2;
+// console.log(sum);
+// console.log(typeof sum);
+//number이라고 나옴
+
+//-----문자형
+// const str1 = "1";
+// const str2 = "2";
+// console.log(typeof str1);
+
+// const num = 1000;
+// const str = "원";
+
+// const counter = num + str;
+// console.log(typeof counter);
+// // ==>문자열로 나옴
+
+// const num3 = 1000;
+// const str3 = "2000";
+
+// const counter1 = num3 + str3;
+// console.log(typeof counter1);
+//문자로 나옴. 숫자 + 문자 = 문자로 나옴
+
+//----형변환
+// prompt("얼마를 원하세요?");
+//숫자로 받아도 문자로 인지함. 이를 형 변환이라고 한다.
