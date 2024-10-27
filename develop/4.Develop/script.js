a29f643a11798b95647ad253a0a0dee425f51fc2
kim5537 <fudgml778@naver.com>
2024-10-27 16:19:53 +0900

    241027

--- develop/4.Develop/script.js
+++ develop/4.Develop/script.js
@@ -1,4 +1,15 @@
 //top
+const TechnologyUl = document.querySelectorAll(".TechnologyUl > li ");
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  const header = document.querySelector("header");
+  if (scroll > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
 
 $(document).ready(function () {
   $(".slider1,.slider2,.slider3,.slider4").slick({
@@ -16,13 +27,59 @@ $(document).ready(function () {
   });
 });
 
-//design
-// $(".fade").slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: "linear",
-//   autoplay: true,
-//   autoplaySpeed: 1000,
-// });
+TechnologyUl.forEach((it) => {
+  const text = it.querySelector(".text");
+  text.addEventListener("click", () => {
+    it.classList.toggle("active");
+  });
+});
+
+$(document).ready(function () {
+  $(".Designslid ").slick({
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    touchMove: false,
+    swipeToSlide: false,
+    cssEase: "linear",
+    fade: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+  $(".textSlide").slick({
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    touchMove: false,
+    swipeToSlide: false,
+    cssEase: "linear",
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+});
+
+$(".moreSlide").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1220,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 860,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
