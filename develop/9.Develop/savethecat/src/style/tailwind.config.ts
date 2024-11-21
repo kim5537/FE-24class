ffd6fcbd4cfe4035a259ebd830334aa4ad51b0dc
kim5537 <fudgml778@naver.com>
2024-11-21 23:18:23 +0900

    24121

---
+++ develop/9.Develop/savethecat/src/style/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000",
+        white: "#f6f6f6",
+        gray: "#D9D9D9",
+      },
+    },
+  },
+};
